ebeb3311e3b935920e495c23e9b67f17d4f69c10
saurav188 <sauravthakur188@gmail.com>
2021-01-19 16:16:35 +0545

    updated images

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ function Header() {
         <div className="welcome"><h1>Welcome,</h1></div>
         <h1 className="name">Saurav Thakur</h1>
         <div className="btns">
-          <div className="resume"><a href="#">Resume</a></div>
+          <div className="resume"><a href="https://drive.google.com/file/d/18tiExMfwK1Cq6BK9xc3c5DOc4jkOi6IR/view">Resume</a></div>
           <div className="github"><a href="https://github.com/saurav188">Github</a></div>
         </div>
     </div>
