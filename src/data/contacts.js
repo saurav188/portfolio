696a47ec629ca2dbb34540bb7fa3b4b089831a51
saurav thakur <sauravthakur188@gmail.com>
2024-04-03 06:49:38 +0545

    Updates

--- src/data/contacts.js
+++
@@ -1,7 +0,0 @@
-const contacts = [
-  {social: 'email', href: 'mailto:sauravthakur188@gmail.com', text:'sauravthakur188@gmail.com'},
-  {social: 'linkedin', href: 'https://www.linkedin.com/in/saurav-thakur-b366761b5/', text: "in/saurav-thakur-b366761b5"},
-  {social: 'github', href: 'https://github.com/saurav188', text: "/saurav188"}
-];
-
-export default contacts;
--- src/data/recommendation.js
+++
@@ -1,9 +0,0 @@
-const recommendation = [
-  // {
-  //   name: 'Suelen Arruda', 
-  //   message: '"Além de ser uma pessoa talentosíssima e com habilidades técnicas inúmeras, nos presentou com seu carisma, tranquilidade, equilíbrio. Sempre pronto a ajudar."', 
-  //   social: 'https://www.linkedin.com/in/suelen-arruda/'
-  // },
-];
-
-export default recommendation;
